69eed5fbdb73c35231438aa5bbfe0f481e3005a1
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-10-06 17:39:08 +0600

    Updates

---
+++ precache-manifest.de9e73b3ea3f5dc6bf6f9f2a89368194.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12b810c5429e9f79c745d885115633d",
+    "url": "/avrora-test/index.html"
+  },
+  {
+    "revision": "7f8d06d9237a9cb3b70e",
+    "url": "/avrora-test/static/css/main.f4f82111.chunk.css"
+  },
+  {
+    "revision": "727c4594fe69738d296e",
+    "url": "/avrora-test/static/js/2.981141c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/avrora-test/static/js/2.981141c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8d06d9237a9cb3b70e",
+    "url": "/avrora-test/static/js/main.1b16334f.chunk.js"
+  },
+  {
+    "revision": "6d1e6051f097e4b08efa",
+    "url": "/avrora-test/static/js/runtime-main.c3ea4768.js"
+  },
+  {
+    "revision": "a5da3cac794319cdcef0b9bdf4fc8771",
+    "url": "/avrora-test/static/media/pen 1.a5da3cac.svg"
+  },
+  {
+    "revision": "5111941a6fca141c037998f878a0a7bc",
+    "url": "/avrora-test/static/media/plus 1.5111941a.svg"
+  },
+  {
+    "revision": "eb7269c7907c071d7c09966e98aa7fc2",
+    "url": "/avrora-test/static/media/remove 1.eb7269c7.svg"
+  }
+]);
